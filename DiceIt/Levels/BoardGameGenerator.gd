601963737212d693967a158a688ee739e4ede9a7
Ellie Kelemen <37041249+whoisEllie@users.noreply.github.com>
2022-07-16 15:10:29 +0200

    more art + working tile gen

--- DiceIt/Levels/BoardGameGenerator.gd
+++ DiceIt/Levels/BoardGameGenerator.gd
@@ -1,55 +1,65 @@
 extends Area2D
 
-const TILE_AMOUNT = 10
+const TILE_AMOUNT = 100
 const OFFSET = 160
 const START_OFFSET = 80
 const TILE_SIZE = 70
 # Declare member variables here. Examples:
+var tileIDs = []
 var tiles = []
-var objects_dict = {0: "Grass", 1: "Snow"}
+
+var objects_dict = {0: "Grass", 1: "Snow", 2: "Random"}
 var grass_texture
 var snow_texture
+var randomblock_texture
+var finish_texture
+var start_texture
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# generates random array of tiles for the playing board
+	var start_sprite = Sprite.new()
+	add_child(start_sprite)
+	start_texture = preload("res://Assets/Start-Block.png")
+	start_sprite.set_texture(start_texture)
+	start_sprite.set_scale(Vector2(1, 1))
+	start_sprite.position = Vector2(START_OFFSET, ProjectSettings.get_setting("display/window/size/height")/2)
+	
+	
+	
 	for i in range (TILE_AMOUNT):
-		tiles.append(randi() % 2)
-		print(objects_dict[tiles[i]])
-		if tiles[i] == 0:
-			var grass_block = RectangleShape2D.new()
-			grass_block.set_extents(Vector2(TILE_SIZE, TILE_SIZE))
-			var collision = CollisionShape2D.new()
-			collision.set_shape(grass_block)
-			collision.position = Vector2(START_OFFSET + (OFFSET * i), ProjectSettings.get_setting("display/window/size/height")/2)
-			print(String(OFFSET * i))
-			add_child(collision)
-			
+		tileIDs.append(randi() % 3)
+		print(objects_dict[tileIDs[i]])
+		if tileIDs[i] == 0:	
 			var grass_sprite = Sprite.new()
 			add_child(grass_sprite)
 			grass_texture = preload("res://Assets/grass.png")
 			grass_sprite.set_texture(grass_texture)
-			grass_sprite.set_scale(Vector2(2.3, 2.3))
-			grass_sprite.position = Vector2(START_OFFSET + (OFFSET * i), ProjectSettings.get_setting("display/window/size/height")/2)
+			grass_sprite.set_scale(Vector2(1, 1))
+			grass_sprite.position = Vector2(START_OFFSET + (OFFSET * tiles.size()), ProjectSettings.get_setting("display/window/size/height")/2)
+			tiles.append(grass_sprite)
 			
 			
-		if tiles[i] == 1:
-			var snow_block = RectangleShape2D.new()
-			snow_block.set_extents(Vector2(TILE_SIZE, TILE_SIZE))
-			var collision = CollisionShape2D.new()
-			collision.set_shape(snow_block)
-			collision.position = Vector2(START_OFFSET + (OFFSET * i), ProjectSettings.get_setting("display/window/size/height")/2)
-			print(String(OFFSET * i))
-			add_child(collision)
-			
+		if tileIDs[i] == 1:		
 			var snow_sprite = Sprite.new()
 			add_child(snow_sprite)
 			snow_texture = preload("res://Assets/snow.png")
 			snow_sprite.set_texture(snow_texture)
-			snow_sprite.set_scale(Vector2(2.3, 2.3))
-			snow_sprite.position = Vector2(START_OFFSET + (OFFSET * i), ProjectSettings.get_setting("display/window/size/height")/2)
+			snow_sprite.set_scale(Vector2(1, 1))
+			snow_sprite.position = Vector2(START_OFFSET + (OFFSET * tiles.size()), ProjectSettings.get_setting("display/window/size/height")/2)
+			tiles.append(snow_sprite)
 
+		if tileIDs[i] == 2:			
+			var randomblock_sprite = Sprite.new()
+			add_child(randomblock_sprite)
+			randomblock_texture = preload("res://Assets/Random-Block.png")
+			randomblock_sprite.set_texture(randomblock_texture)
+			randomblock_sprite.set_scale(Vector2(1, 1))
+			randomblock_sprite.position = Vector2(START_OFFSET + (OFFSET * tiles.size()), ProjectSettings.get_setting("display/window/size/height")/2)
+			tiles.append(randomblock_sprite)
+	
+	print(tiles)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- DiceIt/Levels/BoardGameScene.gd
+++ DiceIt/Levels/BoardGameScene.gd
@@ -1,14 +1,33 @@
 extends Node2D
 
-
+var active_tile = 0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-		
+	yield(get_tree(), "idle_frame")
+	$BoardGamePlayer.transform = $Area2D.tiles[0].get_transform()
+	roll_dice(3)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		active_tile = active_tile + 1
+		if range($Area2D.tiles.size()).has(active_tile):
+			$BoardGamePlayer.transform = $Area2D.tiles[active_tile].get_transform()
+		else:
+			print("finished")
+
+
+func roll_dice(amount):
+	var spaces_to_move = 0
+	for i in range (amount):
+		var add = int(rand_range(1, 7))
+		spaces_to_move += add
+		print(String(add))
+	print("Added random " + String(spaces_to_move))
+	return spaces_to_move
