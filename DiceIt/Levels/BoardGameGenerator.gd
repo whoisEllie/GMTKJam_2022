050987310caa0b6d342af2e3ec5a3c80ec48e675
Ellie Kelemen <37041249+whoisEllie@users.noreply.github.com>
2022-07-16 16:24:02 +0200

    working interp movement + final-ish tile gen?

--- DiceIt/Levels/BoardGameGenerator.gd
+++ DiceIt/Levels/BoardGameGenerator.gd
@@ -3,17 +3,17 @@ extends Area2D
 const TILE_AMOUNT = 100
 const OFFSET = 160
 const START_OFFSET = 80
-const TILE_SIZE = 70
+const TILE_SCALE = 2.3
 # Declare member variables here. Examples:
 var tileIDs = []
 var tiles = []
 
 var objects_dict = {0: "Grass", 1: "Snow", 2: "Random"}
-var grass_texture
-var snow_texture
-var randomblock_texture
-var finish_texture
-var start_texture
+var grass_texture = preload("res://Assets/grass.png")
+var snow_texture = preload("res://Assets/snow.png")
+var randomblock_texture = preload("res://Assets/Random-Block.png")
+var finalmblock_texture = preload("res://Assets/Finish-Block.png")
+var start_texture = preload("res://Assets/Start-Block.png")
 
 
 # Called when the node enters the scene tree for the first time.
@@ -21,10 +21,10 @@ func _ready():
 	# generates random array of tiles for the playing board
 	var start_sprite = Sprite.new()
 	add_child(start_sprite)
-	start_texture = preload("res://Assets/Start-Block.png")
 	start_sprite.set_texture(start_texture)
-	start_sprite.set_scale(Vector2(1, 1))
+	start_sprite.set_scale(Vector2(TILE_SCALE, TILE_SCALE))
 	start_sprite.position = Vector2(START_OFFSET, ProjectSettings.get_setting("display/window/size/height")/2)
+	tiles.append(start_sprite)
 	
 	
 	
@@ -34,9 +34,8 @@ func _ready():
 		if tileIDs[i] == 0:	
 			var grass_sprite = Sprite.new()
 			add_child(grass_sprite)
-			grass_texture = preload("res://Assets/grass.png")
 			grass_sprite.set_texture(grass_texture)
-			grass_sprite.set_scale(Vector2(1, 1))
+			grass_sprite.set_scale(Vector2(TILE_SCALE, TILE_SCALE))
 			grass_sprite.position = Vector2(START_OFFSET + (OFFSET * tiles.size()), ProjectSettings.get_setting("display/window/size/height")/2)
 			tiles.append(grass_sprite)
 			
@@ -44,20 +43,25 @@ func _ready():
 		if tileIDs[i] == 1:		
 			var snow_sprite = Sprite.new()
 			add_child(snow_sprite)
-			snow_texture = preload("res://Assets/snow.png")
 			snow_sprite.set_texture(snow_texture)
-			snow_sprite.set_scale(Vector2(1, 1))
+			snow_sprite.set_scale(Vector2(TILE_SCALE, TILE_SCALE))
 			snow_sprite.position = Vector2(START_OFFSET + (OFFSET * tiles.size()), ProjectSettings.get_setting("display/window/size/height")/2)
 			tiles.append(snow_sprite)
 
 		if tileIDs[i] == 2:			
 			var randomblock_sprite = Sprite.new()
 			add_child(randomblock_sprite)
-			randomblock_texture = preload("res://Assets/Random-Block.png")
 			randomblock_sprite.set_texture(randomblock_texture)
-			randomblock_sprite.set_scale(Vector2(1, 1))
+			randomblock_sprite.set_scale(Vector2(TILE_SCALE, TILE_SCALE))
 			randomblock_sprite.position = Vector2(START_OFFSET + (OFFSET * tiles.size()), ProjectSettings.get_setting("display/window/size/height")/2)
 			tiles.append(randomblock_sprite)
+			
+	var finalblock_sprite = Sprite.new()
+	add_child(finalblock_sprite)
+	finalblock_sprite.set_texture(finalmblock_texture)
+	finalblock_sprite.set_scale(Vector2(TILE_SCALE, TILE_SCALE))
+	finalblock_sprite.position = Vector2(START_OFFSET + (OFFSET * tiles.size()), ProjectSettings.get_setting("display/window/size/height")/2)
+	tiles.append(finalblock_sprite)
 	
 	print(tiles)
 
--- DiceIt/Levels/BoardGameScene.gd
+++ DiceIt/Levels/BoardGameScene.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
-var active_tile = 0
+var target_tile = 0
+var current_tile = 0
+
+var target_position = Vector2(80.0, ProjectSettings.get_setting("display/window/size/height")/2)
+
+var time = 0
+var duration = 1 # length of the interpolation
 
 
 # Called when the node enters the scene tree for the first time.
@@ -16,11 +22,22 @@ func _ready():
 
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
-		active_tile = active_tile + 1
-		if range($Area2D.tiles.size()).has(active_tile):
-			$BoardGamePlayer.transform = $Area2D.tiles[active_tile].get_transform()
+		target_tile = target_tile + roll_dice(2)
+		move_forward_one()
+		
+func move_forward_one():
+	if current_tile < target_tile:
+		if range($Area2D.tiles.size()).has(current_tile+1):
+			target_position = $Area2D.tiles[current_tile+1].position
+			current_tile += 1
+			$Timer.connect("timeout", self, "move_forward_one")
+			$Timer.set_wait_time(duration)
+			$Timer.start()
 		else:
 			print("finished")
+			$Timer.clear()
+	else:
+		pass
 
 
 func roll_dice(amount):
@@ -31,3 +48,6 @@ func roll_dice(amount):
 		print(String(add))
 	print("Added random " + String(spaces_to_move))
 	return spaces_to_move
+
+func _physics_process(delta):
+	$BoardGamePlayer.position = $BoardGamePlayer.position.linear_interpolate(target_position, 0.1)
